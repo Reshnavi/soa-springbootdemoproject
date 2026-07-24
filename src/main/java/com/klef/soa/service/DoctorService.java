01fbd97a2fa544bfac3b33002b600d70cd83a88e
Reshnavi <bareddyreshnavidevi@gmail.com>
2026-07-24 08:47:15 +0530

    Project v2.0

--- src/main/java/com/klef/soa/service/DoctorService.java
+++ src/main/java/com/klef/soa/service/DoctorService.java
@@ -1,12 +1,13 @@
 package com.klef.soa.service;
 import java.util.List;
-
 import com.klef.soa.entity.Doctor;
 public interface DoctorService 
 {
 	Doctor addDoctor(Doctor d);
 	Doctor updateDoctor(Doctor d);
 	List<Doctor> displayAllDoctors();
-	
+	Doctor displayDoctorById(long id);
+	String deleteDoctorById(long id);
+	List<Doctor> displayDoctorByGender(String gender);
 
 }
