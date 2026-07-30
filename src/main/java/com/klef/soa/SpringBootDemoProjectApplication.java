c5a1c78c69f5efc22c945fd14175b387b9318e2e
Reshnavi <bareddyreshnavidevi@gmail.com>
2026-07-30 17:16:29 +0530

    Implemented RestTemplate

--- src/main/java/com/klef/soa/SpringBootDemoProjectApplication.java
+++ src/main/java/com/klef/soa/SpringBootDemoProjectApplication.java
@@ -1,7 +1,11 @@
 package com.klef.soa;
 
+import java.beans.JavaBean;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringBootDemoProjectApplication {
@@ -12,4 +16,13 @@ public class SpringBootDemoProjectApplication {
 		System.out.println("Project is running...!!!");
 	}
 
+@Bean
+
+public RestTemplate restTemplate()
+{
+	return new RestTemplate();
+	
+}
+
+	
 }
